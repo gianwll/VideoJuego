a84c15ab5d269790b8adcace0f2fdb9dcd701512
gianwll <cruzgiancarla@gmail.com>
2023-10-28 22:01:10 -0400

    Subida del controller VideoJuego

---
+++ VideoJuegoController/VideoJuegoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Universidad2.Context;
+using Universidad2.Models;
+
+namespace Universidad2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoJuegoController : Controller
+    {
+        public readonly AplicacionContext _aplicacionContext;
+        public VideoJuegoController(AplicacionContext context)
+        {
+            _aplicacionContext = context;
+        }
+        [HttpPost]
+        [Route("AgregarVideoJuego")]
+        public async Task<IActionResult> Post([FromBody] VideoJuego videoJuego)
+        {
+            _aplicacionContext.VideoJuego.Add(videoJuego);
+            await _aplicacionContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "Agregado");
+        }
+        [HttpGet]
+        [Route("MostrarVideoJuego")]
+        public async Task<IActionResult> Get()
+        {
+            List<VideoJuego> listaVideoJuegos = _aplicacionContext.VideoJuego.OrderByDescending(e => e.idVideoJuego).ToList();
+            return StatusCode(StatusCodes.Status200OK, listaVideoJuegos);
+        }
+        [HttpPut]
+        [Route("EditarVideoJuego/")]
+        public async Task<IActionResult> Put([FromBody] VideoJuego videoJuego)
+        {
+            _aplicacionContext.VideoJuego.Update(videoJuego);
+            await _aplicacionContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "Editado correctamente");
+        }
+        [HttpDelete]
+        [Route("EliminarVideoJuego/")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            VideoJuego videoJuego = _aplicacionContext.VideoJuego.Find(id);
+            _aplicacionContext.VideoJuego.Remove(videoJuego);
+            await _aplicacionContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "Eliminado correctamente");
+
+        }
+    }
+}
