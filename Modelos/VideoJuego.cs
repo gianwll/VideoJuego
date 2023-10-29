87d5c70c9d1d962026d842804a0f1bc62f72f1ef
gianwll <cruzgiancarla@gmail.com>
2023-10-28 22:23:32 -0400

    subida de los modelos de videojuego

---
+++ Modelos/VideoJuego.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Universidad2.Models
+{
+    public class VideoJuego
+    {
+        [Key]
+        public int idVideoJuego { get; set; }
+        public string nombre { get; set; }
+        public string tipoDePago { get; set; }
+        public bool esGrupal { get; set; }
+        public string tipo { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
